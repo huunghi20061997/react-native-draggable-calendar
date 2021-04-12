31c47bd933e609a7e07de0d5b01deca29a14ef63
huunghi20061997 <33802560+huunghi20061997@users.noreply.github.com>
2021-04-12 21:34:24 +0700

    update code

--- src/Day.js
+++ src/Day.js
@@ -80,23 +80,23 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 20,
-    backgroundColor: '#4291EF'
+    backgroundColor: '#FFC701'
   },
   beginDayContainer: {
     height: 40,
     borderTopLeftRadius: 20,
     borderBottomLeftRadius: 20,
-    backgroundColor: '#4291EF'
+    backgroundColor: '#FFC701'
   },
   middleDayContainer: {
     height: 40,
-    backgroundColor: '#4291EF'
+    backgroundColor: '#FFC701'
   },
   endDayContainer: {
     height: 40,
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
-    backgroundColor: '#4291EF'
+    backgroundColor: '#FFC701'
   },
   dayText: {
     fontSize: 15,
